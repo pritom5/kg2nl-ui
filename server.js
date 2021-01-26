dfc6eb38285f568a59ce88f329a4b2af64bce1ca
bonvamby <ambuj.mondal@outlook.com>
2021-01-27 00:30:32 +0100

    changed server.js file for index.html path

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/kg2nl-frontend'));
+app.use(express.static(__dirname + '/dist/kg2nl-ui'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + '/dist/kg2nl-frontend/index.html'));
+res.sendFile(path.join(__dirname + '/dist/kg2nl-ui/index.html'));
 
 });
 
