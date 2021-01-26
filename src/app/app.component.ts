b0add102c8cd8340f9748f11b359856cc7d108ed
pritom5 <74285584+pritom5@users.noreply.github.com>
2021-01-26 15:30:47 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormArray } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'kg2nl-ui';
+  title       = 'app';
+  output_data = ""
+  
+  exampleForm = new FormGroup ({
+    input: new FormControl(),
+  });
+
+  formSubmit() {
+    console.log(this.exampleForm.controls.input.value);
+    this.output_data = "Loading...";
+    
+    setTimeout(() => 
+    {
+      (async () => {
+        const url        = new URL('http://dummy.restapiexample.com/api/v1/employees');
+        const response   = await fetch( url.toString());
+        const data       = await response.json();
+        console.log(data.data[0]);
+        this.output_data = data.data[0].id;
+      })()
+    },
+    2000);
+  }
 }
